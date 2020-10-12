c3b8bd291ce2a5991f14741cbe267beeed8d88ef
Stepan Usatiuk <stepanusatiuk@gmail.com>
2020-10-12 18:35:39 +0300

    use AuthWrapper for routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,17 @@
 import * as React from "react";
-import { connect } from "react-redux";
-import {
-    Redirect,
-    Route,
-    RouteComponentProps,
-    Switch,
-    withRouter,
-} from "react-router";
+import { Route, RouteComponentProps, Switch, withRouter } from "react-router";
 import { AuthScreen } from "~Auth/AuthScreen";
+import { requireAuth } from "~Auth/AuthWrapper";
 import { Home } from "~Home/Home";
-import { Landing } from "~Landing/Landing";
-import { IAppState } from "~redux/reducers";
 
-interface IAppComponentProps extends RouteComponentProps {
-    loggedIn: boolean;
-}
-
-export function AppComponent(props: IAppComponentProps) {
-    const { loggedIn } = props;
-    return loggedIn ? (
+export function AppComponent(props: RouteComponentProps) {
+    return (
         <Switch>
             <Route path="/signup" component={AuthScreen} />,
             <Route path="/login" component={AuthScreen} />,
-            <Route path="/docs/:id" component={Home} />,
-            <Route path="/" component={Home} />,
-        </Switch>
-    ) : (
-        <Switch>
-            <Route path="/signup" component={AuthScreen} />
-            <Route path="/login" component={AuthScreen} />
-            <Route exact={true} path="/" component={Landing} />
-            <Route path="/" component={() => <Redirect to="/login" />} />
+            <Route path="/" component={requireAuth(Home)} />,
         </Switch>
     );
 }
 
-function mapStateToProps(state: IAppState) {
-    return { loggedIn: state.auth.jwt !== null };
-}
-
-export const App = withRouter(connect(mapStateToProps)(AppComponent));
+export const App = withRouter(AppComponent);
