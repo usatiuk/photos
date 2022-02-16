7a53661c2eb3efa0a459947773d637005304a740
Stepan Usatiuk <stepanusatiuk@gmail.com>
2022-02-16 18:16:15 +0000

    fix jest tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -8,9 +8,6 @@ module.exports = {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
             "<rootDir>/src/fileMock.ts",
         "\\.(css|less|scss)$": "<rootDir>/src/styleMock.ts",
-        ...pathsToModuleNameMapper(compilerOptions.paths, {
-            prefix: "<rootDir>/src/",
-        }),
         "react-spring/renderprops":
             "<rootDir>/node_modules/react-spring/renderprops.cjs",
         "react-spring": "<rootDir>/node_modules/react-spring/web.cjs",
