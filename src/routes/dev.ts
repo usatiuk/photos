7a53ebe8a47191a25233283260158b6d128a66c9
Stepan Usatiuk <stepanusatiuk@gmail.com>
2022-04-30 21:58:22 +0200

    workaround photos not loading bug

--- src/routes/dev.ts
+++ src/routes/dev.ts
@@ -4,7 +4,7 @@ import { User } from "~entity/User";
 
 export const devRouter = new Router();
 
-devRouter.post("/dev/clean", async (ctx) => {
+devRouter.get("/dev/clean", async (ctx) => {
     await Photo.remove(await Photo.find());
     await User.remove(await User.find());
     ctx.body = { success: true };
