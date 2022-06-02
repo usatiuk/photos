66a2ef2260c2f2ea3359a774c384255c14d371c7
Stepan Usatiuk <stepanusatiuk@gmail.com>
2022-06-02 15:58:44 +0000

    add proxy for api in dev so it works in codespaces hopefully didn't break anything

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,12 +1,11 @@
 export const apiRoot =
     process.env.API_ROOT ||
-    process.env.NODE_ENV === "production" ||
-    !process.env.NODE_ENV
+    (!process.env.NODE_ENV || process.env.NODE_ENV === "production"
         ? window.location.origin
-        : "http://localhost:3000";
+        : process.env.NODE_ENV === "development" &&
+          window.location.origin + "/api");
 export const webRoot =
     process.env.WEB_ROOT ||
-    process.env.NODE_ENV === "production" ||
-    !process.env.NODE_ENV
+    (!process.env.NODE_ENV || process.env.NODE_ENV === "production"
         ? window.location.origin
-        : "http://localhost:1234";
+        : "http://localhost:1234");
