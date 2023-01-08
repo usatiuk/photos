648909d756cfbb50915f4476fca4f18f7b35e3c8
Stepan Usatiuk <stepan@usatiuk.com>
2023-01-08 20:33:19 +0100

    do not use parcel's proxy because it hangs on photo upload

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,9 +1,15 @@
+// export const apiRoot =
+//     process.env.API_ROOT ||
+//     (!process.env.NODE_ENV || process.env.NODE_ENV === "production"
+//         ? window.location.origin
+//         : process.env.NODE_ENV === "development" &&
+//           window.location.origin + "/api");
+
 export const apiRoot =
     process.env.API_ROOT ||
     (!process.env.NODE_ENV || process.env.NODE_ENV === "production"
         ? window.location.origin
-        : process.env.NODE_ENV === "development" &&
-          window.location.origin + "/api");
+        : process.env.NODE_ENV === "development" && "http://localhost:3000");
 export const webRoot =
     process.env.WEB_ROOT ||
     (!process.env.NODE_ENV || process.env.NODE_ENV === "production"
